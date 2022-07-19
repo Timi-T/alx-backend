d9b4dd378793608f6f73f613fd0f7c433a16b13f
Timi-T <timiopebode@gmail.com>
2022-07-19 12:04:16 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Creating an i18n application
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config():
+    """Configuration class for babel"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object('1-app.Config')
+babel = Babel(app)
+
+
+@app.route("/")
+def home():
+    """Home page"""
+    return render_template('3-index.html', home_header="Hello world",
+                           home_title="Welcome to Holberton")
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
