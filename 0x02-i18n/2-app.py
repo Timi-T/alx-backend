fc59209374c2245c44961e571670280a4d009bb4
Timi-T <timiopebode@gmail.com>
2022-07-19 11:04:51 +0100

    language selector method

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -9,24 +9,26 @@ from flask_babel import Babel, _
 
 class Config():
     """Configuration class for babel"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
-app.config(Config())
+app.config.from_object('1-app.Config')
 babel = Babel()
+
+
+@app.route("/")
+def home():
+    """Home page"""
+    return render_template('1-index.html')
 
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route("/")
-def home():
-    return render_template('0-index.html', home_header=_("opeyemi"))
 
 
 if __name__ == "__main__":
