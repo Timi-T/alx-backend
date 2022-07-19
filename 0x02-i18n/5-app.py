9c30db09a2b31dd735bef75840c2066679dc4046
Timi-T <timiopebode@gmail.com>
2022-07-19 16:35:15 +0100

    emoved argument to jinja template

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -51,7 +51,7 @@ def before_request():
 @app.route("/")
 def home():
     """Home page"""
-    return render_template('5-index.html', user=g.user)
+    return render_template('5-index.html')
 
 
 @babel.localeselector
