805d0efbaeef263b1e6870931d37f15a6cda58db
Timi-T <timiopebode@gmail.com>
2022-07-19 17:25:36 +0100

    config tweak for task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -7,6 +7,9 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
+app = Flask(__name__)
+
+
 class Config():
     """Configuration class for babel"""
 
@@ -15,8 +18,6 @@ class Config():
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
-app.config.from_object('1-app.Config')
 babel = Babel(app)
 
 
@@ -32,7 +33,7 @@ def get_locale(locale=None):
     locale = request.args.get('locale')
     if locale and locale in Config.LANGUAGES:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
