ef65f2e0105fd4147e33c4faef0942c593cedfa9
Timi-T <timiopebode@gmail.com>
2022-07-20 08:34:40 +0100

    documentation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Creating an i18n application
+Creating an i18n application that supports different languages
 """
 
 from flask import Flask, render_template, request
@@ -21,13 +21,13 @@ babel = Babel(app)
 
 @app.route("/")
 def home() -> str:
-    """Home page"""
+    """Home page that displays a simple message"""
     return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Select Language"""
+    """Select Language for the current user"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
